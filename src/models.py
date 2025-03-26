333a55a472a52fe7cd2c9ea8509005c5eff6cc18
Marcelo Cambero <camberotje@gmail.com>
2025-03-26 14:47:24 +0000

    ejercicio hecho con miguel

--- src/models.py
+++ src/models.py
@@ -1,19 +1,37 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, Date, Column, ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
+
+class User (db.Model):
+    __tablename__ = 'User'  
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+
+    conection_favorites5 = relationship("Favorites", back_populates="conection_favorites5", cascade="all, delete-orphan")
+
+    def serialize(self):
+        return {
+        "planet_id": self.planet_id,
+        "specie_id": self.specie_id,
+        "vehicle_id": self.vehicle_id,
+        "character_id": self.character_id
+         }
 
 class Characters (db.Model):
     __tablename__ = 'Characters'  
 
-    character_id = Column(Integer, ForeignKey("Favorites.character_id") primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True, nullable=False)
     height = Column(Integer, nullable=False) 
     date_of_birth = Column(Date, nullable=False)  
     gender = Column(String(20), nullable=False) 
 
-    conection_favorites = relationship("Favorites", back_populates="conection_characters")
+    conection_favorites = relationship("Favorites", back_populates="conection_characters", cascade="all, delete-orphan")
     
 
     def serialize(self):
@@ -26,17 +44,17 @@ class Characters (db.Model):
         }
     
 
-class vehicles (db.Model):
+class Vehicles (db.Model):
     __tablename__ = 'Vehicles'  
 
-    vehicles_id = Column(Integer, ForeignKey("Favorites.vehicle_id") primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True, nullable=False)
     model = Column(String, nullable=False) 
     manufacturer = Column(String(50), nullable=False) 
     length = Column(Integer, nullable=False) 
     max_speed = Column(Integer, nullable=False)
 
-    conection_favorites2 = relationship("Favorites", back_populates="conection_vehicles")
+    conection_favorites2 = relationship("Favorites", back_populates="conection_vehicles", cascade="all, delete-orphan")
 
     
     def serialize(self):
@@ -52,14 +70,14 @@ class vehicles (db.Model):
 class Species (db.Model):
     __tablename__ = 'Species'  
 
-    specie_id = Column(Integer, ForeignKey("Favorites.specie_id") primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True, nullable=False)
     classification = Column(String(50), nullable=False) 
     average_height = Column(Integer, nullable=False) 
     language = Column(String(50), nullable=False) 
     homeworld = Column(String(50), nullable=True)
 
-    conection_favorites3 = relationship("Species", back_populates="conection_species")
+    conection_favorites3 = relationship("Favorites", back_populates="conection_species", cascade="all, delete-orphan")
 
     
     def serialize(self):
@@ -72,17 +90,17 @@ class Species (db.Model):
         "homeworld": self.homeworld 
          }
     
-    class Planets (db.Model):
-        __tablename__ = 'Planets'  
+class Planets (db.Model):
+    __tablename__ = 'Planets'  
 
-    planet_id = Column(Integer, ForeignKey("Favorites.planet_id") primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True, nullable=False)
     diameter = Column(Integer, nullable=False) 
     rotation_period = Column(Integer, nullable=False) 
     gravity = Column(String(50), nullable=False) 
     population = Column(Integer, nullable=True)
 
-    conection_favorites4 = relationship("Planets", back_populates="conection_planets")
+    conection_favorites4 = relationship("Favorites", back_populates="conection_planets", cascade="all, delete-orphan")
 
     
     def serialize(self):
@@ -95,17 +113,18 @@ class Species (db.Model):
         "population": self.population
          }
     
-    class Favorites (db.Model):
-        __tablename__ = 'Favorites'  
-    
-    id = Column(Integer, primary_key=True)
-    planet_id = Column(Integer, primary_key=True)
-    specie_id = Column(Integer, primary_key=True)
-    vehicle_id = Column(Integer, primary_key=True)
-    character_id = Column(Integer, primary_key=True)
+class Favorites (db.Model):
+    __tablename__ = 'Favorites'  
 
-    conection_characters = relationship("Character", back_populates="conection_favorites")
-    conection_vehicles = relationship("vehicles", back_populates="conection_favorites2")
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("User.id"), nullable=False)
+    planet_id = Column(Integer, ForeignKey("Planets.id"), nullable=True)
+    specie_id = Column(Integer, ForeignKey("Species.id"), nullable=True)
+    vehicle_id = Column(Integer, ForeignKey("Vehicles.id"), nullable=True)
+    character_id = Column(Integer, ForeignKey("Characters.id"), nullable=True)
+
+    conection_characters = relationship("Characters", back_populates="conection_favorites")
+    conection_vehicles = relationship("Vehicles", back_populates="conection_favorites2")
     conection_species = relationship("Species", back_populates="conection_favorites3")
     conection_planets = relationship("Planets", back_populates="conection_favorites4")
     conection_users = relationship("User", back_populates="conection_favorites5")
@@ -118,23 +137,7 @@ class Species (db.Model):
         "character_id": self.character_id
          }
     
-    class User (db.Model):
-        __tablename__ = 'User'  
     
-    user_id = Column(Integer, ForeignKey("Favorites.id") primary_key=True)
-    username = Column(String(50), unique=True, nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
-    password = Column(String(255), nullable=False)
-
-    conection_favorites5 = relationship("User", back_populates="conection_favorites5")
-
-    def serialize(self):
-        return {
-        "planet_id": self.planet_id,
-        "specie_id": self.specie_id,
-        "vehicle_id": self.vehicle_id,
-        "character_id": self.character_id
-         }
     
 
     
